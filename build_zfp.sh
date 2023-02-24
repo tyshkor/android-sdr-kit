e21e680fe89e80603443b5994163665c31d83cfb
Oleksandr Pravdyvyi <awxrvtb@gmail.com>
2023-02-24 19:34:11 +0200

    fix: build zfp script updated

--- build_zfp.sh
+++ build_zfp.sh
@@ -7,15 +7,17 @@ build_zfp() { # [arch] [android_abi] [compiler_abi]
     mkdir build  
     cd build  
     cmake ..  
-    cmake --build . --config Release  
-    cp ./lib/libzfp.so.1.0.0 $SDR_KIT_ROOT/x86/lib
-    cp ./lib/libzfp.so.1.0.0 $SDR_KIT_ROOT/x86_64/lib
-    cp ./lib/libzfp.so.1.0.0 $SDR_KIT_ROOT/armeabi-v7a/lib
-    cp ./lib/libzfp.so.1.0.0 $SDR_KIT_ROOT/arm64-v8a/lib
-    mv $SDR_KIT_ROOT/x86/lib/libzfp.so.1.0.0 $SDR_KIT_ROOT/x86/lib/libzfp.so
-    mv $SDR_KIT_ROOT/x86_64/lib/libzfp.so.1.0.0 $SDR_KIT_ROOT/x86_64/lib/libzfp.so
-    mv $SDR_KIT_ROOT/armeabi-v7a/lib/libzfp.so.1.0.0 $SDR_KIT_ROOT/armeabi-v7a/lib/libzfp.so
-    mv $SDR_KIT_ROOT/arm64-v8a/lib/libzfp.so.1.0.0 $SDR_KIT_ROOT/arm64-v8a/lib/libzfp.so
+    cmake --build . --config Release
+    pushd lib
+    echo "===================== lib ====================="
+    ls
+    popd
+    echo "Coping libzfp.so into ${SDR_KIT_ROOT}/*/lib 4 times"
+    cp ./lib/libzfp.so $SDR_KIT_ROOT/x86/lib
+    cp ./lib/libzfp.so $SDR_KIT_ROOT/x86_64/lib
+    cp ./lib/libzfp.so $SDR_KIT_ROOT/armeabi-v7a/lib
+    cp ./lib/libzfp.so $SDR_KIT_ROOT/arm64-v8a/lib
+    echo "Coping sucsessfull"
     cd ../../
 }
 build_zfp
