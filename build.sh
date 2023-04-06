12d873f0a6321b6845b75446b6985630c910b775
tyshkor <tyshko1@gmail.com>
2023-04-06 22:15:50 +0200

    comment out ar

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ load_android_toolchain() { # [arch] [compiler_abi]
     export CC="$ANDROID_NDK_TOOLCHAIN/$1-linux-android$2$ANDROID_API_LEVEL-clang"
     export CXX="$ANDROID_NDK_TOOLCHAIN/$1-linux-android$2$ANDROID_API_LEVEL-clang++"
     export AS="$ANDROID_NDK_TOOLCHAIN/$1-linux-android$2$ANDROID_API_LEVEL-as"
-    export AR="$ANDROID_NDK_TOOLCHAIN/$1-linux-android$2$ANDROID_API_LEVEL-ar"
+    # export AR="$ANDROID_NDK_TOOLCHAIN/$1-linux-android$2$ANDROID_API_LEVEL-ar"
     
     # echo "The contents of ANDROID_NDK_TOOLCHAIN directory are:"
     # echo "$(ls -al $ANDROID_NDK_TOOLCHAIN)"
@@ -18,7 +18,7 @@ load_native_toolchain() { # [arch] [compiler_abi]
     export CC="gcc"
     export CXX="g++"
     export AS="as"
-    export AR="ar"
+    # export AR="ar"
 }
 
 gen_cmake_args() { # [android_abi]
