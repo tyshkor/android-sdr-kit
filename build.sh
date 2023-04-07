4839772cafd06d4f54163190084df6a87cc8318d
tyshkor <tyshko1@gmail.com>
2023-04-07 09:33:05 +0200

    more build_adder after lib dir creation

--- build.sh
+++ build.sh
@@ -102,22 +102,6 @@ rustup target add \
 
 mkdir $SDR_KIT_ROOT/rust_shared_lib
 
-# Build adder
-build_adder() { # [arch] [android_abi] [compiler_abi]
-    echo "===================== adder ($3) ====================="
-    load_android_toolchain $1 $4
-    cd adder
-    mkdir $SDR_KIT_ROOT/rust_shared_lib/$3
-    cargo build --release --target-dir $SDR_KIT_ROOT/rust_shared_lib/$3
-    cp $SDR_KIT_ROOT/rust_shared_lib/$3/release/libadder.so $SDR_KIT_ROOT/$3/lib/libadder.so
-    #  --target $2
-    cd ..
-}
-build_adder i686 i686-linux-android x86
-build_adder x86_64 x86_64-linux-android x86_64
-build_adder armv7a armv7-linux-androideabi armeabi-v7a eabi
-build_adder aarch64 aarch64-linux-android arm64-v8a
-
 # Build ZSTD
 build_zstd() { # [arch] [android_abi] [compiler_abi]
     echo "===================== ZSTD ($2) ====================="
@@ -195,6 +179,22 @@ build_libusb() {
     cd ..
 }
 build_libusb
+
+# Build adder
+build_adder() { # [arch] [android_abi] [compiler_abi]
+    echo "===================== adder ($3) ====================="
+    load_android_toolchain $1 $4
+    cd adder
+    mkdir $SDR_KIT_ROOT/rust_shared_lib/$3
+    cargo build --release --target-dir $SDR_KIT_ROOT/rust_shared_lib/$3
+    cp $SDR_KIT_ROOT/rust_shared_lib/$3/release/libadder.so $SDR_KIT_ROOT/$3/lib/libadder.so
+    #  --target $2
+    cd ..
+}
+build_adder i686 i686-linux-android x86
+build_adder x86_64 x86_64-linux-android x86_64
+build_adder armv7a armv7-linux-androideabi armeabi-v7a eabi
+build_adder aarch64 aarch64-linux-android arm64-v8a
 
 # Build volk
 build_volk() { # [android_abi]
