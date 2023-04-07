7853af9ab7a22c5eddcf214badb2baa12243064f
tyshkor <tyshko1@gmail.com>
2023-04-07 14:17:58 +0200

    fix target

--- build.sh
+++ build.sh
@@ -188,8 +188,7 @@ build_adder() { # [arch] [android_abi] [compiler_abi]
     cd adder
     mkdir $SDR_KIT_ROOT/rust_shared_lib/$3
     cargo build --release --target-dir $SDR_KIT_ROOT/rust_shared_lib/$3
-    cp $SDR_KIT_ROOT/rust_shared_lib/$3/release/libadder.so $SDR_KIT_ROOT/$3/lib/libadder.so
-    #  --target $2
+    cp $SDR_KIT_ROOT/rust_shared_lib/$3/release/libadder.so $SDR_KIT_ROOT/$3/lib/libadder.so --target $2
     cd ..
 }
 build_adder i686 i686-linux-android x86
