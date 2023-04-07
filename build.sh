c2127961f2bc8a86ed6ace5ff93ddd7ac2feba30
tyshkor <tyshko1@gmail.com>
2023-04-07 15:23:45 +0200

    add ANDROID_NDK

--- build.sh
+++ build.sh
@@ -6,6 +6,7 @@ load_android_toolchain() { # [arch] [compiler_abi]
     export CC="$ANDROID_NDK_TOOLCHAIN/$1-linux-android$2$ANDROID_API_LEVEL-clang"
     export CXX="$ANDROID_NDK_TOOLCHAIN/$1-linux-android$2$ANDROID_API_LEVEL-clang++"
     export AS="$ANDROID_NDK_TOOLCHAIN/$1-linux-android$2$ANDROID_API_LEVEL-as"
+    export ANDROID_NDK="$ANDROID_NDK_TOOLCHAIN"
 }
 
 load_native_toolchain() { # [arch] [compiler_abi]
