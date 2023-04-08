44970b6c5dbe2fb7ff20afd2f62f47c60854a950
tyshkor <tyshko1@gmail.com>
2023-04-08 18:23:18 +0200

    attempt 6

--- build.sh
+++ build.sh
@@ -2,7 +2,8 @@ set -e
 
 sh -c 'echo "deb http://deb.debian.org/debian stretch main\ndeb-src http://deb.debian.org/debian stretch main" >> /etc/apt/sources.list'
 
-apt-get install -t stretch debian-archive-keyring
+wget http://ftp.debian.org/debian/pool/main/d/debian-archive-keyring/debian-archive-keyring_2019.1_all.deb
+dpkg -i debian-archive-keyring_2019.1_all.deb
 apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 04EE7237B7D453EC 648ACFD622F3D138 0E98404D386FA1D9 EF0F382A1A7B6500
 
 apt-get update -y
