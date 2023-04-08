bc7b934ec676d408b802128344a2399cd75dead5
tyshkor <tyshko1@gmail.com>
2023-04-08 14:10:07 +0200

    gcc-multilib

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 set -e
 
-apt install -y wget git p7zip-full default-jdk build-essential cmake patchelf python3-mako build-essential pkg-config libssl-dev clang libclang-dev libc6-dev
+apt install -y wget git p7zip-full default-jdk build-essential cmake patchelf python3-mako build-essential pkg-config libssl-dev clang libclang-dev libc6-dev gcc-multilib
 
 # Helper functions
 load_android_toolchain() { # [arch] [compiler_abi]
